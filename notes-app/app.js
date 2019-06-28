121f0e4113cc31231ab9b4129df58538d26dbfdb
Sergio L. Benítez D <sergio.kun21@gmail.com>
2019-06-28 16:31:18 -0500

    S03: Install chalk package to five colors to the logs message.

--- notes-app/app.js
+++ notes-app/app.js
@@ -5,8 +5,12 @@ fs.writeFileSync('notes.txt', 'My name is sergio')
 fs.appendFileSync('notes.txt', ' I live in Bogotá')
 */
 const validator = require('validator')
-const getNotes = require('./notes.js')
-const msg = getNotes()
+const chalk = require('chalk')
 
-console.log(msg)
+const getNotes = require('./notes.js')
+
+const msg = getNotes()
+const greenMsg = chalk.green.inverse.bold(msg);
+
+console.log(greenMsg)
 console.log(validator.isURL('https/mead.io'))
