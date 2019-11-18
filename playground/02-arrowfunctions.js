40c6dd9d11f8cf0c7e940cd55b4eef02a80aff87
Sergio L. Benítez D <sergio.kun21@gmail.com>
2019-11-17 21:47:03 -0500

    SXX: Add playground folder with arrow functions examples

---
+++ playground/02-arrowfunctions.js
@@ -0,0 +1,35 @@
+/**
+ * Arrrow function
+ */
+
+
+// const square = function (x) {
+//     return x * x;
+// }
+
+
+const square = (x) => {
+    return x * x;
+}
+
+console.log(square(2)) // Print: 4
+
+// shorthand syntax
+const squareAlt = (x) => x * x;
+
+console.log(squareAlt(3)) // Print: 9
+
+// Objects
+const event = {
+    name: 'Brithday Party',
+    guestList: ['Tino, Pibe, Tren'],
+    printGuestList() {
+        console.log('Guest list for ' + this.name);
+
+        this.guestList.forEach((guest) => {
+            console.log(guest + 'is attending' + this.name);
+        });
+    }
+}
+
+event.printGuestList();
