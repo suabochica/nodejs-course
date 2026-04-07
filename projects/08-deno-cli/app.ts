1d271b0f71f627ebd95cb89023a4a5f9443f0580
Sergio L. Benítez D. <sergio.kun21@gmail.com>
2026-04-07 17:11:14 -0500

    deno: crush ts

---
+++ projects/08-deno-cli/app.ts
@@ -0,0 +1,10 @@
+// Variables
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const num = 23; // implicit type
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+let str: string; // explicit type
+
+// Functions
+export function multiply(a:number, b:number) {
+    return a * b
+}
---
+++ projects/08-deno-cli/other.ts
@@ -0,0 +1,3 @@
+import { helloWorld } from './main.ts'
+
+helloWorld()
