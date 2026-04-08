14a561b71148aada9de6f5c6e5ab90c529c3197e
Sergio L. Benítez D. <sergio.kun21@gmail.com>
2026-04-08 16:51:41 -0500

    deno: init shortener url project

---
+++ projects/09-deno-shortener-url/src/main.ts
@@ -0,0 +1,25 @@
+import { serveDir } from "@std/http";
+
+const userPagePattern = new URLPattern({ pathname: "/users/:id" });
+const staticPathPattern = new URLPattern({ pathname: "/static/*" });
+
+export default {
+  fetch(req) {
+    const url = new URL(req.url);
+
+    if (url.pathname === "/") {
+      return new Response("Home page");
+    }
+
+    const userPageMatch = userPagePattern.exec(url);
+    if (userPageMatch) {
+      return new Response(userPageMatch.pathname.groups.id);
+    }
+
+    if (staticPathPattern.test(url)) {
+      return serveDir(req);
+    }
+
+    return new Response("Not found", { status: 404 });
+  },
+} satisfies Deno.ServeDefaultExport;
