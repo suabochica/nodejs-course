69ad5b755fefbbfca04bc859a32b40bf0cc3f310
Sergio L. Benítez D. <sergio.kun21@gmail.com>
2026-04-08 17:47:33 -0500

    shortener: use deno-kv

---
+++ projects/09-deno-shortener-url/src/db.ts
@@ -0,0 +1,77 @@
+
+import { encodeBase64Url } from "jsr:@std/encoding";
+import { crypto } from "jsr:@std/crypto/crypto";
+
+export type ShortLink = {
+  shortCode: string;
+  longUrl: string;
+  createdAt: number
+  userId: string;
+  clickCount: number;
+  lastClickEvent?: string
+}
+
+export async function generateShortCode(longUrl: string) {
+  try {
+    new URL(longUrl);
+  } catch (error) {
+    console.log(error);
+    throw new Error("Invalid URL provided");
+  }
+
+  // Generate a unique identifier for the URL
+  const urlData = new TextEncoder().encode(longUrl + Date.now());
+  const hash = await crypto.subtle.digest("SHA-256", urlData);
+
+  // Take the first 8 of the hash for the short URL
+  const shortCode = encodeBase64Url(hash.slice(0, 8));
+
+  return shortCode;
+}
+
+const kv = await Deno.openKv();
+
+export async function storeShortLink(
+  longUrl: string,
+  shortCode: string,
+  userId: string,
+) {
+  const shortLinkKey = ["shortlinks", shortCode];
+  const data: ShortLink = {
+    shortCode,
+    longUrl,
+    createdAt: Date.now(),
+    userId,
+    clickCount: 0,
+  };
+
+  const res = await kv.set(shortLinkKey, data);
+
+  if (!res.ok) {
+    // Handle error, e.g., log it or throw an exception
+    throw new Error(`Failed to store short link;`)
+  }
+
+  return res
+}
+
+export async function getShortLink(shortCode: string) {
+  const link = await kv.get<ShortLink>(["shortlinks", shortCode]);
+
+  return link.value
+}
+
+// Temporary example
+// deno run -A --unstable-kv src/db.ts
+
+const longUrl = "https://fireship.io";
+const shortCode = await generateShortCode(longUrl)
+const userId = "test-user";
+
+console.log(shortCode)
+
+await storeShortLink(longUrl, shortCode, userId);
+
+const linkData = await getShortLink(shortCode);
+
+console.log(linkData)
